35e5b6701e7a63af279ba41542331747d37c73c4
Sadra Saderi <sadrassx@gmail.com>
2021-07-13 15:38:20 +0430

    Add navbar
    Co-Authored-By: Mohammad Danesh <mohammad@danesh.dev>

--- res/js/main.js
+++ res/js/main.js
@@ -1,1 +1,29 @@
-console.log('Hello!');
+function parseVal(str) {
+    let res = str.match(/(-?[\d.]+)([a-z%]*)/);
+    return {
+        val: parseFloat(res[1]),
+        unit: res[2]
+    }
+}
+
+$(function () {
+    let $navbar = $('#navbar');
+    let margin = parseVal($navbar.css('margin-top'));
+
+    $(document).scroll(function () {
+        if ($(this).scrollTop() < margin.val / 1.5) {
+            if ($(this).scrollTop() < 4) {
+                $navbar.css({
+                    marginTop: margin.val + margin.unit,
+                    marginBottom: margin.val + margin.unit
+                });
+            }
+        }
+        else {
+            $navbar.css({
+                marginTop: (margin.val / 4) + margin.unit,
+                marginBottom: (margin.val / 4) + margin.unit
+            });
+        }
+    });
+});
