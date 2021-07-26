2fc2084ca109d829a2f3c7a7a825db4a462fa9ca
Sadra Saderi <sadrassx@gmail.com>
2021-07-26 15:49:36 +0430

    Add dummy sections

---
+++ res/js/main-fullpage.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+    let navMargin = parseVal($navbar.css('margin-top'));
+
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors: ['top', 'cabin-panels', 'floor-panel', 'contact-us'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: [
+            'صفحه اصلی',
+            'پنل داخل کابین',
+            'پنل طبقات',
+            'تماس با ما'
+        ],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        lazyLoading: true,
+
+        //events
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 1 && nextIndex != 1) {
+                navExpand();
+                $navbar.css({
+                    marginTop: (navMargin.val / 4) + navMargin.unit,
+                    marginBottom: (navMargin.val / 4) + navMargin.unit
+                });
+            } else if (nextIndex == 1) {
+                navShrink();
+                $navbar.css({
+                    marginTop: navMargin.val + navMargin.unit,
+                    marginBottom: navMargin.val + navMargin.unit
+                });
+            }
+        },
+        afterLoad: function (anchorLink, index) { },
+        afterRender: function () { },
+        afterResize: function () { },
+        afterResponsive: function (isResponsive) { },
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) { },
+        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
+    });
+});
--- res/js/navbar.js
+++ res/js/navbar.js
@@ -1,66 +1,79 @@
-$(document).ready(function () {
-    let $navbar = $('#navbar');
-    let $content = $('#nav-content');
-    let $center = $('#nav-center');
-    let $side = $('.nav-side');
+var $navbar = $('#navbar');
 
+var $navContent = $('#nav-content');
+var $navCenter = $('#nav-center');
+var $navSide = $('.nav-side');
+
+function navExpand() {
+    $navbar.addClass('expand');
+    $navContent.addClass('expand');
+
+    $navContent.removeClass('col-6');
+    $navContent.addClass('col-12');
+
+    $navCenter.removeClass('col-12');
+    $navCenter.addClass('col-6');
+
+    $navSide.removeClass('col-0');
+    $navSide.addClass('col-3');
+}
+
+function navShrink() {
+    $navbar.removeClass('expand');
+    $navContent.removeClass('expand');
+
+    $navContent.removeClass('col-12');
+    $navContent.addClass('col-6');
+
+    $navCenter.removeClass('col-6');
+    $navCenter.addClass('col-12');
+
+    $navSide.removeClass('col-3');
+    $navSide.addClass('col-0');
+}
+
+$(document).ready(function () {
     let $expandHeight = $($navbar.attr('expand-height-target'));
     let expandHeight = parseFloat($navbar.attr('expand-height'));
 
     if ($expandHeight && !expandHeight)
         expandHeight = $expandHeight.height();
 
+    let locked = $navbar.attr('expand-locked');
+
+    if (!locked) locked = false;
+    else locked = (locked === 'true' || locked === '1');
+
     let margin = parseVal($navbar.css('margin-top'));
 
-    let expand = function () {
-        $navbar.addClass('expand');
-        $content.addClass('expand');
-
-        $content.removeClass('col-6');
-        $content.addClass('col-12');
-
-        $center.removeClass('col-12');
-        $center.addClass('col-6');
-
-        $side.removeClass('col-0');
-        $side.addClass('col-3');
-    };
-
-    let shrink = function () {
-        $navbar.removeClass('expand');
-        $content.removeClass('expand');
-
-        $content.removeClass('col-12');
-        $content.addClass('col-6');
-
-        $center.removeClass('col-6');
-        $center.addClass('col-12');
-
-        $side.removeClass('col-3');
-        $side.addClass('col-0');
-    };
 
     let navScroll = function () {
-        if ($(document).scrollTop() < margin.val / 1.5) {
-            if ($(document).scrollTop() < 4) {
+        if (!locked) {
+            if ($(document).scrollTop() < margin.val / 1.5) {
+                if ($(document).scrollTop() < 4) {
+                    $navbar.css({
+                        marginTop: margin.val + margin.unit,
+                        marginBottom: margin.val + margin.unit
+                    });
+                }
+            } else {
                 $navbar.css({
-                    marginTop: margin.val + margin.unit,
-                    marginBottom: margin.val + margin.unit
+                    marginTop: (margin.val / 4) + margin.unit,
+                    marginBottom: (margin.val / 4) + margin.unit
                 });
             }
+
+            if (expandHeight) {
+                if ($(document).scrollTop() > expandHeight) navExpand();
+                else navShrink();
+            } else {
+                navExpand();
+            }
         } else {
-            $navbar.css({
-                marginTop: (margin.val / 4) + margin.unit,
-                marginBottom: (margin.val / 4) + margin.unit
-            });
+            if ($navbar.hasClass('expand')) navExpand();
+            else navShrink();
         }
 
-        if (expandHeight) {
-            if ($(document).scrollTop() > expandHeight) expand();
-            else shrink();
-        } else {
-            expand();
-        }
     };
     navScroll();
 
