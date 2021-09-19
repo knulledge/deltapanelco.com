495bc4b95c25ed71cb622cf8a7e7efb1f35b08c3
Mohammad Danesh <mohammad@danesh.dev>
2021-09-19 15:49:37 +0430

    Add animated scroll indicator

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        indicator: 'indicator 1s infinite',
+      },
+      keyframes: {
+        indicator: {
+          '0%, 100%': {
+            'transform': 'translateY(0)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)'
+          },
+          '50%': {
+            'transform': 'translateY(18px)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)'
+          }
+        }
+      }
+    },
   },
   variants: {
     extend: {},
