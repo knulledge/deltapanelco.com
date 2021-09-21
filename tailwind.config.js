54380c3b460e26f5fb1018a8fa5063b90eaa7192
Mohammad Danesh <mohammad@danesh.dev>
2021-09-21 20:05:36 +0430

    Animate arrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       animation: {
         wave: 'wave 48s infinite linear',
         indicator: 'indicator 1s infinite',
+        arrow: 'arrow 1s infinite',
       },
       keyframes: {
         wave: {
@@ -19,6 +20,7 @@ module.exports = {
             'transform': 'rotate(360deg)',
           },
         },
+
         indicator: {
           '0%, 100%': {
             'transform': 'translateY(0)',
@@ -26,6 +28,17 @@ module.exports = {
           },
           '50%': {
             'transform': 'translateY(18px)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)'
+          }
+        },
+
+        arrow: {
+          '0%, 100%': {
+            'transform': 'translateX(0)',
+            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)'
+          },
+          '50%': {
+            'transform': 'translateX(8px)',
             'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)'
           }
         }
